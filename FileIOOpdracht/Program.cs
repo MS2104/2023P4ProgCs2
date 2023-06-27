b040e81f7f04a97a6072984ee624bdf7c56951cc
MS2104 <31002@ma-web.nl>
2023-06-27 22:44:07 +0200

    Alles tot 11 af ish

---
+++ FileIOOpdracht/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FileIOOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string[] content = File.ReadAllLines("leesdezefile.txt");
+
+            foreach (string line in content)
+            {
+                Console.WriteLine(line);
+            }
+
+            string[] content2 = new string[] { "ASTROWOLRD is beroofd van de Album of the Year 2019" };
+            File.WriteAllText("C:\\School M3\\2023P4ProgCs2\\FileIOOpdracht\\bin\\Debug\\net7.0\\output\\mijnnieuwefile.txt", "ASTROWORLD >>> Invasion of Privacy");
+
+            string[] lines = { "", "Baby I'm a firework", "h" };
+            File.AppendAllLines("C:\\School M3\\2023P4ProgCs2\\FileIOOpdracht\\mijnnieuwefile.txt", lines);
+
+            Directory.CreateDirectory("output");
+        }
+    }
+}
---
+++ SplitOpdracht/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+namespace SplitOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string content = File.ReadAllText("TextFile1.txt");
+
+            // System.IO.FileNotFoundException: 'Could not find file 'C:\School M3\2023P4ProgCs2\SplitOpdracht\bin\Debug\net7.0\TextFile1.txt'.'
+            // Ik heb TextFile1.txt maar hier naar toe verplaatst.
+
+            string[] keyvalue = content.Split(":");
+
+            foreach (string key in keyvalue)
+            {
+                Console.WriteLine(key);
+            }
+            
+            string[] cijfers = keyvalue[1].Split(",");
+
+            foreach (string cijfer in cijfers)
+            {
+                Console.WriteLine(cijfer);
+            }
+        }
+    }
+}
